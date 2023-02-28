44105c09fc094f112cef83d84a8a7f030dd7e9d8
chainan33 <126548574+chainan33@users.noreply.github.com>
2023-02-28 13:39:36 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,30 @@
+def validate_number(number):
+    if type(number) != int:
+        if type(number) == str:
+            return "input integer"
+        if number >= 1 and number <= 12:
+            return "input integer"
+        else:
+            return "out of range"
+    elif number >= 1 and number <= 12:
+        return number
+    else:
+        return "out of range"
+
+
+def number_to_month(number):
+    month = ["january", "february", "march", "april", "may", "june", "july",
+             "august", "september", "october", "november", "december"]
+    # if number == 1:
+    #     return "january"
+    # else:
+    #     return "february"
+    return month[number-1]
+
+
+def display_month(number):
+    result = validate_number(number)
+    if type(result) == int:
+        return number_to_month(result)
+    else:
+        return result
